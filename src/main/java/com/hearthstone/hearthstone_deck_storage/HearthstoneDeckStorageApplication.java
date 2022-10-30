1e061fcfdfb02e030feb5f59579cc008ddbeb90a
Illia Sydorchuk <batonvagon@gmail.com>
2022-10-30 16:59:22 +0200

    Initial commit

---
+++ src/main/java/com/hearthstone/hearthstone_deck_storage/HearthstoneDeckStorageApplication.java
@@ -0,0 +1,13 @@
+package com.hearthstone.hearthstone_deck_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearthstoneDeckStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HearthstoneDeckStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hearthstone/hearthstone_deck_storage/HearthstoneDeckStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hearthstone.hearthstone_deck_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearthstoneDeckStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
